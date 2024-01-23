199b9dab33b66061683811b3572c9398be5c04d2
leilapanova <144453728+leilapanova@users.noreply.github.com>
2024-01-23 15:26:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,246 @@
+// if (14<9){ 
+//     console.log('yes') 
+// } else if (15<=2){ 
+//     console.log('no') 
+// }else if (15>15){ 
+//     console.log('wevwrvt') 
+// }else{ 
+//     console.log('qerver') 
+// } 
+// a = 15 if 2<3 else 14 
+// let a = (2>3) ? 15 :(5>6) ? 25 : (8-3 > 2) ? 47 : "error" 
+// console.log(a) 
+ 
+// if (a>15 && a>100){ 
+//     console.log('erverf') 
+// }else{ 
+//     console.log('3242134') 
+// } 
+// let is_auth = false 
+// console.log(is_auth && 'efvergtbe') 
+ 
+// let a = 1 
+// switch(a){ 
+//     case 1: 
+//         console.log('rvewwrv'); 
+//         break; 
+//     case 2: 
+//         console.log(234253); 
+//     default: 
+//         console.log('error') 
+//         break; 
+// } 
+ 
+// let lang = '1'; 
+ 
+// rus = ['пон', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'] 
+// eng = ['mon', 'tu','wen', 'th', 'fr', 'sat', 'sun'] 
+ 
+// if (lang === 1){ 
+//     console.log(rus) 
+// }else{ 
+//     console.log(eng) 
+// } 
+ 
+ 
+ 
+ 
+//Работа с if-else 
+                                            //1 
+// let a = 1 
+ 
+// if (a === 0){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 0 
+// if (a === 0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = -3 
+// if (a === 0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+ 
+                                                 //2 
+// let a = 1 
+ 
+// if (a>0){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 0 
+// if (a>0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = -3 
+// if (a>0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+                                                    //3 
+// let a = 1 
+// if (a<0){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 0 
+// if (a<0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = -3 
+// if (a<0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+                                        //4 
+// let a = 1 
+// if (a>=0){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 0 
+// if (a>=0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = -3 
+// if (a>=0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+ 
+ 
+                                        //5 
+// let a = 1 
+// if (a<=0){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 0 
+// if (a<=0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = -3 
+// if (a<=0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+ 
+ 
+ 
+                                        //6 
+// let a = 1 
+// if (a!==0){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 0 
+// if (a!==0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = -3 
+// if (a!==0){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+ 
+ 
+ 
+                                        //7 
+// let a = 'тест' 
+// if (a === 'test'){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 'test' 
+// if (a === 'test'){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = -3 
+// if (a === 'test'){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+ 
+ 
+ 
+                                        //8 
+// let a = '1' 
+// if (a === '1'){ 
+//     console.log('Верно') 
+// } else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 1 
+// if (a === '1'){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+// a = 3 
+// if (a === '1'){ 
+//     console.log('Верно') 
+// }else{ 
+//     console.log('Неверно') 
+// } 
+ 
+ 
+ 
+ 
+                    //Работа с && (и) и || (или) 1 
+// let a = 5 
+// if (a > 0 && a < 5){ 
+//     console.log('Верно') 
+//
